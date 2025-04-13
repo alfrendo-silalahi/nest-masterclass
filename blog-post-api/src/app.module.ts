6a2d13f565f967b048ad53ff8a0777a81f3cd633
Alfrendo Silalahi <alfrendos72@gmail.com>
2025-04-13 19:55:19 +0700

    add typeorm dependency and connect to postgres db

--- blog-post-api/src/app.module.ts
+++ blog-post-api/src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: false,
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      database: 'nestjs_masterclass_blog_post',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- blog-post-api/src/users/users.service.ts
+++ blog-post-api/src/users/users.service.ts
@@ -4,32 +4,53 @@ import { UpdateUserDto } from './dtos/request/update-user.request.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { GetUserRequestDto } from './dtos/response/get-user.response.dto';
 
+/**
+ * Class to connect to users table and perform business operations
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * The constructor
+   */
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /**
+   * The method to get all the users from the database
+   */
   getUsers(page: number, limit: number): GetUserRequestDto[] {
     console.log(page, limit);
     this.authService.isAuth();
     return users;
   }
 
+  /**
+   * The method to get user by id
+   */
   getUser(id: number): string {
     return `user with id ${id}`;
   }
 
+  /**
+   * The method to create new user
+   */
   createUser(createUserDto: CreateUserDto): string {
     return JSON.stringify(createUserDto);
   }
 
+  /**
+   * The method to update existing user
+   */
   updateUser(id: number, updateUserDto: UpdateUserDto): string {
     return `ID ${id} || ${JSON.stringify(updateUserDto)}`;
   }
 }
 
+/**
+ * Temporary data for users
+ */
 const users: GetUserRequestDto[] = [
   {
     firstName: 'Alfrendo Silalahi',
